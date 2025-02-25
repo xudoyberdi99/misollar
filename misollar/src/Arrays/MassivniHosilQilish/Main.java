e8fd746e5833cf4b3a6362965d640b740da1eb70
Xudoyberdi <xudoyberdi3003@gmail.com>
2025-02-25 07:31:20 +0500

    'Array 25.02.2025'

--- misollar/src/Arrays/MassivniHosilQilish/Main.java
+++ misollar/src/Arrays/MassivniHosilQilish/Main.java
@@ -8,8 +8,13 @@ public class Main {
 //        Array1(5);
 //        ArifProgress(5);
 //        FibonacciArray(5);
-        FibonacciArray(2);
+//        FibonacciArray(2);
+//        Array6(5,1,2);
+//        ReversevArray(6);
+//        Array13(7);
+        Array15(10);
     }
+
 
     // Array-1
     public static void Array1(int n){
@@ -58,6 +63,81 @@ public class Main {
 
         for (int i = 0; i < a.length; i++) {
             System.out.println(a[i]);
+        }
+    }
+
+    //Array-6
+    public static void Array6(int n,int a, int b){
+        int[] massiv = new int[n];
+        massiv[0]=a;
+        massiv[1]=b;
+        int sum=massiv[0]+massiv[1];
+        for (int i = 2; i < n; i++) {
+            massiv[i]=sum;
+            sum=sum+massiv[i];
+        }
+
+        for (int i = 0; i < massiv.length; i++) {
+            System.out.println(massiv[i]);
+        }
+    }
+
+    //Array-7
+    public static void ReversevArray(int n){
+        int[] massiv = new int[n];
+        for (int i = 0; i < n; i++) {
+            massiv[i]=i;
+        }
+        for (int i = massiv.length-1; i>=0 ; i--) {
+            System.out.println(massiv[i]);
+        }
+    }
+
+    //Array-13
+    public static void Array13(int n){
+        int[] massiv = new int[n];
+        for (int i = 0; i < n; i++) {
+            massiv[i]=i;
+        }
+        for (int i = massiv.length-1; i >=0 ; i=i-2) {
+            System.out.println(massiv[i]);
+        }
+
+        for (int i = 0; i < massiv.length; i++) {
+            System.out.print(massiv[i]);
+        }
+    }
+
+    //Array-15
+    public static void Array15(int n){
+        int[] massiv = new int[n];
+        for (int i = 0; i < n; i++) {
+            massiv[i]=i;
+        }
+
+       for (int i = 1; i < massiv.length; i+=2) {
+           System.out.println(i+ " " + massiv[i]);
+       }
+       System.out.println(" ");
+       System.out.println(" ");
+       System.out.println(" ");
+       System.out.println(" ");
+
+       for (int i = massiv.length-2; i >=0 ; i-=2) {
+           System.out.println(i+" " +massiv[i]);
+       }
+   }
+
+   //Array-16
+    public static void Array16(int n){
+        int[] massiv = new int[n];
+        for (int i = 0; i < n; i++) {
+            massiv[i]=i;
+        }
+
+        for (int i = 0; i < massiv.length/2; i++) {
+            System.out.println(massiv[i]);
+            System.out.println(massiv[massiv.length-i]);
         }
     }
 }
